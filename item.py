a5426bb91ce1c75da551e351e140ddfbeb626a95
monzag <zaganiacz.m@gmail.com>
2017-08-08 21:00:24 +0200

    Add constructor

---
+++ item.py
@@ -0,0 +1,10 @@
+class Item:
+
+    def __init__(self, name, quantity, unit, price_per_unit, categories, is_bought=False):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+        self.price_per_unit = price_per_unit
+        self.categories = categories
+        self.is_bought = is_bought
+
