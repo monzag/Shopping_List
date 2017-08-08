4f5997b83f32e5fae647f75ef26a669ad1d087a7
monzag <zaganiacz.m@gmail.com>
2017-08-08 21:52:46 +0200

    Add create_from_file, create_shopping_list and create_item

--- shopping_list.py
+++ shopping_list.py
@@ -1,4 +1,5 @@
 from item import Item
+from datetime import datetime
 
 
 class ShoppingList:
@@ -15,3 +16,46 @@ class ShoppingList:
 
     def get_total_price(self):
         return sum(item.get_total_price() for item in self.items)
+
+    @classmethod
+    def create_from_csv(cls, file_name):
+        splitted_data = cls.get_data_from_file(file_name)
+
+        for row in splitted_data:
+            if len(row) < 4:
+                shopping_list = cls.create_shopping_list(file_name, row)
+
+            else:
+                item = cls.create_item(row)
+                shopping_list.items.append(item)
+
+        return shopping_list
+
+    @classmethod
+    def create_shopping_list(cls, file_name, row):
+        name = file_name
+        day, month, year = row[0], row[1], row[2]
+        date = datetime(year, month, day)
+        shopping_list = ShoppingList(name, date)
+
+        return shopping_list
+
+    @classmethod
+    def create_item(cls, row):
+        name, quantity, unit, price_per_unit, categories_list = row[0], row[1], row[2], row[3], row[4]
+
+        categories = categories_list.split('|')
+
+        last_item = -1
+        if row[last_item] == 'done':
+            is_bought = True
+
+        else:
+            is_bought = False
+
+        item = Item(name, quantity, unit, price_per_unit, categoties, is_bought)
+
+        return item
+
+
+
