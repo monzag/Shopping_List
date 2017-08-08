1ac142fce6430f356ebf1c667bd05ddfa7c3f16e
monzag <zaganiacz.m@gmail.com>
2017-08-08 22:15:20 +0200

    Add print_main_menu, get_input, get_list, print_exit_option

---
+++ view.py
@@ -0,0 +1,25 @@
+def print_main_menu():
+    options = ['Show items', 'Show total price']
+    get_list(options)
+    print_exit_option()
+
+
+def get_list(options):
+    print('')
+    number = 1
+    for option in options:
+        print('{}. {}'.format(number, option))
+        number += 1
+    print('')
+
+
+def print_exit_option():
+    print('0. Exit\n')
+
+
+def get_input():
+    number = input('\nWrite number: ')
+    if number.isdigit():
+        return int(number)
+
+    return None
