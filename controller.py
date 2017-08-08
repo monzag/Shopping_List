17888b9f0010d4d6f79f8f38a96fa8f90ce2cc6f
monzag <zaganiacz.m@gmail.com>
2017-08-08 23:30:22 +0200

    Add create shopping list

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from shopping_list import ShoppingList
 from item import Item
+from datetime import datetime
 import view
 
 
@@ -31,6 +32,15 @@ def open_shopping_list_from_file():
         view.print_error_file()
 
 
+def create_shopping_list():
+    name = view.get_shop_name()
+    day, month, year = view.get_date()
+    if day and month and year:
+        date = datetime(year, month, day)
+        shopping_list = ShoppingList(name, date)
+        shopping_menu(shopping_list)
+
+
 def shopping_menu(shopping_list):
     user_choice = ''
     while user_choice != 0:
@@ -42,4 +52,8 @@ def shopping_menu(shopping_list):
 
         elif user_choice == 2:
             print(shopping_list.get_total_price())
+
+        elif user_choice == 3:
+            # TODO add new item
+            pass
 
